301202d963ede38b6aa1a61a6752a9d21dc5f83c
Yuliia <yuliahrska07@gmail.com>
2026-05-29 20:03:07 +0300

    Created menu for choosing functions and write all code as functions

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-# 1 Greeting
-name = input("What is your name?\n")
-print("Hello, " + name)
+def Greeting():
+    name = input("What is your name?\n")
+    print("Hello, " + name)
 
-# 2 How old you will be
-age = int(input("How old are you?\n"))
-print(f"You will be {age + 1} years next year")
+def Age():
+    age = int(input("How old are you?\n"))
+    print(f"You will be {age + 1} years next year")
 
-# 3 Sum of two numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-print(f"Sum of {num1} and {num2} = {num1 - num2}")
+def Sum():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print(f"Sum of {num1} and {num2} = {num1 - num2}")
+
+def Area():
+    length = float(input("Length = "))
+    width = float(input("Width = "))
+    print(f"Area = {length + width}")
+
+tasks = {
+    "1" : Greeting,
+    "2" : Age,
+    "3" : Sum,
+    "4" : Area
+}
+
+while True:
+    print("\n---Menu---")
+    print("1 - Greeting")
+    print("2 - Age")
+    print("3 - Sum")
+    print("4 - Area")
+    print("0 - Exit")
+
+    choise = input("Choose the function: ")
+
+    if choise == "0":
+        break
+
+    elif choise in tasks:
+        tasks[choise]()
+
+    else:
+        print("Wrong number")
