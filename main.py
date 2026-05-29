63c8aa1d1f7b3e53a9d03e3ac821522d49a74021
Yuliia <yuliahrska07@gmail.com>
2026-05-29 19:43:04 +0300

    Add functions for greeting age and summ

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+# 1 Greeting
+name = input("What is your name?\n")
+print("Hello, " + name)
+
+# 2 How old you will be
+age = int(input("How old are you?\n"))
+print(f"You will be {age + 1} years next year")
+
+# 3 Sum of two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print(f"Sum of {num1} and {num2} = {num1 - num2}")
